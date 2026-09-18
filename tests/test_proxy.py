834a3d2e7631104f5a61bc2175663c7a166fad60
Sankalp Thakur <31366524+sankalpsthakur@users.noreply.github.com>
2026-09-18 08:25:43 +0400

    Test ProxyCommand waits for subprocess

--- tests/test_proxy.py
+++ tests/test_proxy.py
@@ -106,6 +106,7 @@ class TestProxyCommand:
         proxy = ProxyCommand("hi")
         proxy.close()
         os_kill.assert_called_once_with(Popen.return_value.pid, signal.SIGTERM)
+        Popen.return_value.wait.assert_called_once_with()
 
     @patch("paramiko.proxy.subprocess.Popen")
     def test_closed_exposes_whether_subprocess_has_exited(self, Popen):
